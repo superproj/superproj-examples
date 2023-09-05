dd5962c30f1cc857f8daed977fd13b14ab4e8bc6
孔令飞 <lkong@tencent.com>
2023-09-05 22:44:18 +0800

    update
    Change-Id: Icae9cdf495702da6ebc767e9402b4d8e229cf782

---
+++ wire/raw/controller.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"time"
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
+// Define controller layer
+type UserCenterService struct {
+	biz BizFactory
+}
+
+func NewUserCenterService(db *gorm.DB) *UserCenterService {
+	// 创建 Biz 层实例
+    biz := NewBiz(db)
+
+	return &UserCenterService{biz: biz}
+}
+
+
+// Define List User API
+type ListUserRequest struct {
+	Limit  int
+	Offset int
+}
+
+type UserReply struct {
+	UserID   string `json:"userID"`
+	Username string `json:"username"`
+	Nickname string `json:"nickname"`
+	Password string `json:"password"`
+	Email    string `json:"email"`
+	Phone    string `json:"phone"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+type ListUserResponse struct {
+	TotalCount int64        `json:"totalCount"`
+	Users      []*UserReply `json:"users"`
+}
+
+func (s *UserCenterService) ListUser(ctx context.Context, req *ListUserRequest)(*ListUserResponse, error) {
+	fmt.Println("ListUser function called")
+	return s.biz.Users().List(ctx, req)
+}
---
+++ wire/raw/go.mod
@@ -0,0 +1,15 @@
+module github.com/superproj/superproj-examples/wire/raw
+
+go 1.20
+
+require (
+	github.com/jinzhu/copier v0.4.0
+	gorm.io/driver/mysql v1.5.1
+	gorm.io/gorm v1.25.4
+)
+
+require (
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+)
