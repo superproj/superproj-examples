5c6fe8746d17125cb98eb140d19148ce3da34d51
孔令飞 <lkong@tencent.com>
2023-09-06 15:16:01 +0800

    updateush
    Change-Id: Iaa073fe76141b699be0c0898fd7ca57d5c7ea8ce

---
+++ wire/fieldsof/store.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+
+	"gorm.io/gorm"
+)
+
+type datastore struct {
+	db *gorm.DB
+}
+
+type DataStoreWrapper struct {
+	ds *datastore
+}
+
+func NewDataStoreWrapper(db *gorm.DB) DataStoreWrapper {
+	return DataStoreWrapper{&datastore{db}}
+}
+
+// 创建 Store 层实例
+func NewStore(db *gorm.DB) *datastore {
+	return &datastore{db}
+}
+
+func (ds *datastore) ListUser(ctx context.Context, offset, limit int) (count int64, ret []*UserM, err error) {
+	ans := ds.db.
+		Offset(offset).
+		Limit(limit).
+		Order("id desc").
+		Find(&ret).
+		Offset(-1).
+		Limit(-1).
+		Count(&count)
+
+	return count, ret, ans.Error
+}
